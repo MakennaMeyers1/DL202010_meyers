cecb1cb170a07c363755fc97b75f019b4e2eba76
MakennaMeyers1 <Makenna_Meyers1@baylor.edu>
2020-04-09 12:42:44 -0500

    trying to run counter_test

---
+++ Lab10/Lab10_7seg_TDM/Lab10_7seg_TDM.srcs/sim_1/new/register_test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/09/2020 12:36:33 PM
+// Design Name: 
+// Module Name: register_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_test(
+
+    );
+endmodule
