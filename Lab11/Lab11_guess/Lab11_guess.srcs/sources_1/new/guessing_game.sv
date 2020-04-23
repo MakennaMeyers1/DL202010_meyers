b23ea8b9f31b19909ef9f378e4a12e9a3e15c6ce
MakennaMeyers1 <Makenna_Meyers1@baylor.edu>
2020-04-23 11:58:32 -0500

    more board testing

--- Lab11/Lab11_guess/Lab11_guess.srcs/sources_1/new/guessing_game.sv
+++ Lab11/Lab11_guess/Lab11_guess.srcs/sources_1/new/guessing_game.sv
@@ -2,9 +2,9 @@
 // ELC 2137 , Makenna Meyers , 2020 -04 -22
 
 
-module guessing_game(
+module guessing_game #( parameter N =21)
 
-    input btnU, btnD, btnL, btnR, btnC, clk, en,
+    ( input btnU, btnD, btnL, btnR, btnC, clk, en,
     input [15:0]sw,
     
     output [6:0]seg,
@@ -19,8 +19,6 @@ module guessing_game(
     wire db3_out;
     wire [3:0] b_in;
     wire y3, y2, y1, y0;
-    wire win;
-    wire lose;
     wire mux_out;
     wire counter_out;
     wire counter_tick;
@@ -28,13 +26,11 @@ module guessing_game(
     
     assign b_in = {db3_out, db2_out, db1_out, db0_out};
     assign led[15:3] = 0;
-    assign an[3:1] = 0;
-    assign seg[4:2] = 0;
    
-    debounce #(.N(21)) db1(.clk(clk), .reset(btnC), .in(btnU), .out(db0_out), .tick(tick));
-    debounce #(.N(21)) db2(.clk(clk), .reset(btnC), .in(btnD), .out(db1_out), .tick(tick));
-    debounce #(.N(21)) db3(.clk(clk), .reset(btnC), .in(btnL), .out(db2_out), .tick(tick));
-    debounce #(.N(21)) db4(.clk(clk), .reset(btnC), .in(btnR), .out(db3_out), .tick(tick));
+    debounce #(.N(21)) db0(.clk(clk), .reset(btnC), .in(btnU), .out(db0_out), .tick(tick));
+    debounce #(.N(21)) db1(.clk(clk), .reset(btnC), .in(btnD), .out(db1_out), .tick(tick));
+    debounce #(.N(21)) db2(.clk(clk), .reset(btnC), .in(btnL), .out(db2_out), .tick(tick));
+    debounce #(.N(21)) db3(.clk(clk), .reset(btnC), .in(btnR), .out(db3_out), .tick(tick));
     guess_FSM #(.N(21)) gfsm(.b(b_in), 
                    .y({{an[0], seg[6]}, {an[0], seg[5]}, {an[0], seg[1]}, {an[0], seg[0]}}),
                    .win(led[1]), .lose(led[0]),
